8680fc43569671e74339df8367cd8c0452239b3a
rotate2050 <rushikesh.joshi@sjsu.edu>
2015-02-19 12:25:37 -0800

    First Test Program for decompiling

---
+++ CS266-SRE/src/FirstProg.java
@@ -0,0 +1,15 @@
+
+public class FirstProg {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i =0;
+		if (i==0) {
+			System.out.println("This is 0");
+		}
+		else {
+			System.out.println("This is not 0");
+		}
+	}
+
+}
